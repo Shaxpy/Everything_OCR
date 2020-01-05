a6129ad927022550db116833b76972d38e186b84
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-01-05 16:27:22 +0530

    Update Imagemagick-OCR.py

--- Imagemagick-OCR.py
+++ Imagemagick-OCR.py
@@ -2,16 +2,18 @@ from PIL import Image
 import pytesseract
 import io
 from wand.image import Image as wi
-pdf=wi(filename="qqq.pdf",resolution=300)
-pdfImage=pdf.convert("jpeg")
+pdfImage=wi(filename="2.png",resolution=300)
+#if needed to be used as pdf reader---
+#pdfImage=pdf.convert("jpeg")
 imageBlobs=[]
 for img in pdfImage.sequence:
     imgPage=wi(image=img)
-    imageBlobs.append(imgPage.make_blob('jpeg'))
+    imageBlobs.append(imgPage.make_blob('png'))
 recog_text=[]
 
 for imgBlob in imageBlobs:
     im=Image.open(io.BytesIO(imgBlob))
-    text=pytesseract.image_to_string(im,lang='eng',config='-c preserve_interword_spaces=1')
+    text=pytesseract.image_to_string(im,lang='eng',config='-c preserve_interword_spaces=1 --oem 1 --psm 6')
+    text= text.replace('\n','')
     recog_text.append(text)
 print(recog_text)
