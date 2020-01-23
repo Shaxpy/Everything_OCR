b6b9f4a46bfda20452b5b17535a2dab7a45dc04f
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-01-23 23:55:09 +0530

    Update Best-OCR.py

--- Best-OCR.py
+++ Best-OCR.py
@@ -5,16 +5,11 @@ import sys
 from pdf2image import convert_from_path 
 import os 
   
-# Path of the pdf 
 PDF_file = "hu2009.pdf"
     
-# Store all the pages of the PDF in a variable 
 pages = convert_from_path(PDF_file, 500) 
-  
-# Counter to store images of each page of PDF to image 
     image_counter = 1
     
-    # Iterate through all the pages stored above 
     for page in pages: 
   
     filename = "page_"+str(image_counter)+".jpg"
