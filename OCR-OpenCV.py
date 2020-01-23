448aab73f903fc396e872962c25395531b2a301d
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-01-23 23:54:30 +0530

    Update OCR-OpenCV.py

--- OCR-OpenCV.py
+++ OCR-OpenCV.py
@@ -5,9 +5,7 @@ from PIL import Image
 import pytesseract
 import argparse
 import cv2
-
 import os
- 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
